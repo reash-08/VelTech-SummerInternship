db609408f6bd12ac1d7cc3b764b40b80486a7bac
reash-08 <reashmar01@gmail.com>
2026-06-12 10:28:51 +0530

    Update app.py

--- Student Registration System/app.py
+++ Student Registration System/app.py
@@ -1,38 +1,73 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
+# Home Page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
 
+# Register Page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    message = ""
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        roll_number = request.form['roll_number']
+        department = request.form['department']
+        year = request.form['year']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        address = request.form['address']
+
+        conn = sqlite3.connect('students.db')
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        INSERT INTO students
+        (name, roll_number, department, year, email, phone, gender, address)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """,
+        (name, roll_number, department, year,
+         email, phone, gender, address))
+
+        conn.commit()
+        conn.close()
+
+        message = "Student Registered Successfully!"
+
+    return render_template('register.html', message=message)
+
+
+# Students Page
 @app.route('/students')
 def students():
-    students_data = [
-        {"roll":101,"name":"Arjun","dept":"IT","year":"3"},
-        {"roll":102,"name":"Priya","dept":"CSE","year":"2"},
-        {"roll":103,"name":"Rahul","dept":"ECE","year":"4"},
-        {"roll":104,"name":"Sneha","dept":"IT","year":"1"},
-        {"roll":105,"name":"Kiran","dept":"EEE","year":"2"},
-        {"roll":106,"name":"Anjali","dept":"CSE","year":"3"},
-        {"roll":107,"name":"Vikram","dept":"MECH","year":"4"},
-        {"roll":108,"name":"Deepika","dept":"IT","year":"1"},
-        {"roll":109,"name":"Surya","dept":"ECE","year":"2"},
-        {"roll":110,"name":"Divya","dept":"CSE","year":"3"}
-    ]
+
+    conn = sqlite3.connect('students.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM students")
+    students_data = cursor.fetchall()
+
+    conn.close()
 
     return render_template(
         'students.html',
         students=students_data
     )
 
+
+# About Page
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
